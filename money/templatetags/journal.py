5d537c205ed0ca3e6c7e9bb6297f654d06335675
Takeshi Nakamura <taqueci.n@gmail.com>
2020-04-05 10:45:29 +0900

    A little bit of refactoring

--- money/templatetags/journal.py
+++ money/templatetags/journal.py
@@ -3,31 +3,34 @@
 from django import template
 from django.utils.translation import gettext_lazy as _
 
-from ..models import Account
+from ..views.shared import journal
 
 register = template.Library()
 
 
 @register.filter
 def journal_category_text(obj):
-    if obj.credit.entry == Account.ENTRY_INCOME:
+    category = journal.category(obj)
+
+    if category == journal.INCOMING:
         return _('Incoming')
-    if obj.debit.entry == Account.ENTRY_EXPENSE:
+    if category == journal.OUTGOING:
         return _('Outgoing')
-    if obj.debit.entry == Account.ENTRY_LIABILITY:
+    if category == journal.REPAYMENT:
         return _('Repayment')
-    
+
     return _('Other')
 
 
 @register.filter
 def journal_category_badge_class(obj):
-    if obj.credit.entry == Account.ENTRY_INCOME:
-        return 'badge badge-success'
-    if obj.debit.entry == Account.ENTRY_EXPENSE:
-        return 'badge badge-danger'
-    if obj.debit.entry == Account.ENTRY_LIABILITY:
-        return 'badge badge-warning'
-    
-    return 'badge badge-secondary'
+    category = journal.category(obj)
 
+    if category == journal.INCOMING:
+        return 'badge badge-success'
+    if category == journal.OUTGOING:
+        return 'badge badge-danger'
+    if category == journal.REPAYMENT:
+        return 'badge badge-warning'
+
+    return 'badge badge-secondary'
---
+++ money/views/shared/journal.py
@@ -0,0 +1,20 @@
+# Copyright (C) Takeshi Nakamura. All rights reserved.
+
+import datetime
+from money.models import Account
+
+UNDEFINED = 0
+INCOMING = 1
+OUTGOING = 2
+REPAYMENT = 3
+
+
+def category(obj):
+    if obj.credit.entry == Account.ENTRY_INCOME:
+        return INCOMING
+    if obj.debit.entry == Account.ENTRY_EXPENSE:
+        return OUTGOING
+    if obj.debit.entry == Account.ENTRY_LIABILITY:
+        return REPAYMENT
+
+    return UNDEFINED
