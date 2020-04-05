e701741cd91ada3aeee217681a49f7ec08cc21ea
Takeshi Nakamura <taqueci.n@gmail.com>
2020-04-05 11:43:01 +0900

    Add journal detail page

--- money/urls.py
+++ money/urls.py
@@ -8,5 +8,6 @@ app_name = 'main'
 urlpatterns = [
     path('journals/', journals.index, name='journals'),
     path('journals/new', journals.new, name='new_journal'),
+    path('journals/<id>', journals.show, name='journal'),
     path('', home.index, name='home'),
 ]
--- money/views/journals.py
+++ money/views/journals.py
@@ -2,7 +2,7 @@
 
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext_lazy as _
 
 from ..forms import JournalForm
@@ -73,6 +73,14 @@ def new(request):
 
     return render(request, 'money/journals/new.html', {
         'debit': account, 'credit': account,
+    })
+
+
+def show(request, id):
+    obj = get_object_or_404(Journal, pk=id)
+
+    return render(request, 'money/journals/show.html', {
+        'object': obj
     })
 
 
