164043cf8882d509045802dca84e0b3a4100375d
Takeshi Nakamura <taqueci.n@gmail.com>
2020-04-07 23:17:26 +0900

    Update top page

--- money/views/home.py
+++ money/views/home.py
@@ -1,7 +1,38 @@
 # Copyright (C) Takeshi Nakamura. All rights reserved.
 
+import datetime
+
+from django.db.models import F, Sum
 from django.shortcuts import render
+
+from ..models import Account, Journal
+from .shared import chart
+
+INDEX_NUM = 5
 
 
 def index(request):
-    return render(request, 'money/home/index.html', {})
+    today = datetime.date.today()
+
+    start = today.replace(month=1, day=1)
+    end = today.replace(month=12, day=31)
+
+    q = Journal.objects.filter(
+        disabled=False, date__gte=start, date__lte=end
+    )
+
+    summary = q.aggregate(
+        income=Sum('income'), expense=Sum('expense'),
+        balance=Sum(F('income')-F('expense')),
+    )
+
+    outgoing = q.exclude(expense=0).values(
+        'debit__id', 'debit__name'
+    ).annotate(sum=Sum('expense')).order_by('-sum')
+
+    return render(request, 'money/home/index.html', {
+        'start': start, 'end':end,
+        'page': q.order_by('-id')[:INDEX_NUM],
+        'summary': summary, 'outgoing': outgoing,
+        'data_doughnut_outgoing': chart.data_doughnut_outgoing(outgoing),
+    })
