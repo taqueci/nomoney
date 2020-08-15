f9eb9dafd08ae297d584c7806b9369685d9dfb2f
Takeshi Nakamura <taqueci.n@gmail.com>
2020-08-15 22:34:20 +0900

    Add screen for changing password

--- system/urls.py
+++ system/urls.py
@@ -3,10 +3,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from .views import PasswordChange, PasswordChangeDone
+
 app_name = 'system'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='system/login.html'),
          name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('password/change/', PasswordChange.as_view(), name='password_change'),
+    path('password/change/done', PasswordChangeDone.as_view(),
+         name='password_change_done'),
 ]
