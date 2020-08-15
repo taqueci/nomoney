ffcc0a84804370be6b31c759e72a88e1ba2bb91f
Takeshi Nakamura <taqueci.n@gmail.com>
2020-08-15 16:26:37 +0900

    Add ability to show user avatar image

--- system/admin.py
+++ system/admin.py
@@ -2,9 +2,32 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.utils.translation import gettext_lazy as _
+
+from . import models
 
 
-@admin.register(User)
+@admin.register(models.User)
 class AdminUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'email', 'image')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups'),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'password1', 'password2',
+                'first_name', 'last_name', 'email'
+            ),
+        }),
+    )
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_superuser',
+        'last_login',
+    )
