f2ae160b48881a9bad788ff6cdbebe00e8e538f1
Takeshi Nakamura <taqueci.n@gmail.com>
2020-08-23 17:58:34 +0900

    Update Docker configuratoins

--- run.sh
+++ run.sh
@@ -1,31 +1,6 @@
 #!/bin/sh
 
-if type python3 > /dev/null 2>&1; then
-    alias python=python3
-    alias pip=pip3
-fi
-
-pip install -r requirements.txt
-
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
-
-# Create super user
-cat <<EOF | python manage.py shell
-from django.contrib.auth import get_user_model
-
-ADMIN_USER = 'admin'
-ADMIN_MAIL = 'admin@example.com'
-ADMIN_PASSWORD = 'password'
-
-User = get_user_model()
-
-if not User.objects.filter(username=ADMIN_USER).exists():
-    User.objects.create_superuser(ADMIN_USER, ADMIN_MAIL, ADMIN_PASSWORD)
-EOF
-
-python manage.py compilemessages
+sh init.sh
 
 exec uwsgi \
     --http-socket :49152 \
