352c35d2871b06c44d08859b198c95b58cc332b2
Takeshi Nakamura <taqueci.n@gmail.com>
2021-08-23 00:16:26 +0900

    Use IntegerChoices class for an account entry

--- money/views/shared/account.py
+++ money/views/shared/account.py
@@ -8,4 +8,4 @@ def grouped_objects():
         'entry': x[0],
         'name': x[1],
         'data': Account.objects.filter(entry=x[0]).order_by('-rank')
-    } for x in Account.ENTRY_CHOICES]
+    } for x in Account.Entry.choices]
