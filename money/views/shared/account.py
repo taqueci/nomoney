4cb93f483313ef6d2271d87106cd2da066ffda94
Takeshi Nakamura <taqueci.n@gmail.com>
2022-02-19 20:43:16 +0900

    Add entry filter

--- money/views/shared/account.py
+++ money/views/shared/account.py
@@ -1,6 +1,6 @@
 # Copyright (C) Takeshi Nakamura. All rights reserved.
 
-from money.models import Account
+from money.models import Account, Journal
 
 
 def grouped_objects():
@@ -9,3 +9,16 @@ def grouped_objects():
         'name': x[1],
         'data': Account.objects.filter(entry=x[0]).order_by('-rank')
     } for x in Account.Entry.choices]
+
+
+def entry_sets():
+    q = Journal.objects.values_list(
+        'debit__entry', 'credit__entry'
+    ).order_by(
+        'debit__entry', 'credit__entry'
+    ).distinct()
+
+    return [{
+        'value': Account.Entry(x[0]).value * 10 + Account.Entry(x[1]).value,
+        'debit': Account.Entry(x[0]), 'credit': Account.Entry(x[1]),
+    } for x in q]
