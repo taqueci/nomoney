cbb9b08483d71e0ad625e6f39d4e344803964516
Takeshi Nakamura <taqueci.n@gmail.com>
2022-05-07 10:09:22 +0900

    Rename template tags

--- money/templatetags/value.py
+++ money/templatetags/value.py
@@ -8,3 +8,15 @@ register = template.Library()
 @register.filter
 def value_percent(num, total):
     return 100 * num / total if total > 0 else 0
+
+
+@register.simple_tag(takes_context=True)
+def value_if_eq_query(context, key, val, value, default=''):
+    vals = context['request'].GET.getlist(key)
+
+    return value if str(val) in vals else default
+
+
+@register.simple_tag
+def value_if_eq(a, b, value, default=''):
+    return value if a == b else default
