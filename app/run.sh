a216c277ca250842dceffc64a8a229d17a8dd99a
Takeshi Nakamura <taqueci.n@gmail.com>
2025-11-03 17:26:04 +0900

    Delete logging settings

--- app/run.sh
+++ app/run.sh
@@ -3,7 +3,6 @@
 exec uwsgi \
     --http-socket :49152 \
     --wsgi-file config/wsgi.py \
-    --logto logs/uwsgi.log \
     --touch-chain-reload uwsgi-reload \
     --master \
     --processes 4 --threads 1 --thunder-lock \
