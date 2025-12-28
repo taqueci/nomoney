716b51c438c9ae8384693a624152269de940d8c0
Takeshi Nakamura <taqueci.n@gmail.com>
2025-12-28 18:40:28 +0900

    Update ability to export Journal data as CSV

--- app/api/views/journals.py
+++ app/api/views/journals.py
@@ -1,12 +1,10 @@
 # Copyright (C) Takeshi Nakamura. All rights reserved.
 
-import csv
-
-from django.http import HttpResponse
 from django.utils.translation import gettext as _
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
+from rest_framework.settings import api_settings
+from rest_framework_csv import renderers
 
 from money.models import Journal
 from money.views.shared.journal import Filter
@@ -16,13 +14,33 @@ from ..serializers import (
 )
 from .shared.permission import HasPermission
 
-INDEX_DEFAULT_SORT = '-date'
+
+class CsvRenderer(renderers.CSVRenderer):
+    header = [
+        'id', 'credit.entry.label', 'debit.entry.label', 'date', 'amount',
+        'summary', 'fy', 'enabled', 'author.full_name', 'created', 'updated',
+    ]
+
+    labels = {
+        'id': 'ID',
+        'credit.entry.label': _('Credit'),
+        'debit.entry.label': _('Debit'),
+        'date': _('Date'),
+        'amount': _('Amount'),
+        'summary': _('Summary'),
+        'fy': 'FY',
+        'enabled': _('Enabled'),
+        'author.full_name': _('Author'),
+        'created': _('Created on'),
+        'updated': _('Updated on'),
+    }
 
 
 # pylint: disable-next=too-many-ancestors
 class JournalViewSet(viewsets.ModelViewSet):
     filterset_class = Filter
     permission_classes = [IsAuthenticated & HasPermission]
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES + [CsvRenderer]
 
     queryset = Journal.objects.all().select_related().prefetch_related(
         'tags', 'attachments',
@@ -37,37 +55,8 @@ class JournalViewSet(viewsets.ModelViewSet):
 
         return JournalSerializer
 
+    def paginate_queryset(self, queryset):
+        if self.request.accepted_renderer.format == CsvRenderer.format:
+            return None
 
-class Export(APIView):
-    def get(self, request):
-        query = _query(request)
-
-        return _response_csv('journals.csv', query)
-
-
-def _query(request):
-    q = Journal.objects.available().order_by(INDEX_DEFAULT_SORT)
-
-    return Filter(request.GET, queryset=q).qs.select_related()
-
-
-def _response_csv(name, query):
-    r = HttpResponse(content_type='text/csv')
-    r['Content-Disposition'] = f'attachment; filename="{name}"'
-
-    w = csv.writer(r)
-    w.writerow([
-        '#', _('Date'), _('Entry'), _('Debit'), _('Credit'),
-        _('Amount'), _('Summary'), _('Note'),
-    ])
-
-    for x in query:
-        entry_d = x.debit.get_entry_display()
-        entry_c = x.credit.get_entry_display()
-
-        w.writerow([
-            x.pk, x.date, f'{entry_d}/{entry_c}', x.debit.name, x.credit.name,
-            x.amount, x.summary, x.note,
-        ])
-
-    return r
+        return super().paginate_queryset(queryset)
